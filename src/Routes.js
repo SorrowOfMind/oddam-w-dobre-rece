efe452818afb79800e7f6a1b8cf2da861c9fe261
Barbara Kumor <b.kumor@yahoo.com>
2020-06-29 19:27:49 +0200

    header, stats

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
+import Home from './components/layout/Home';
 import SignUpForm from './components/auth/SignUpForm';
 import LogInForm from './components/auth/LogInForm';
 
 const Routes = () => {
     return (
         <Switch>
-            {/* <Route exact path='/' component={}/> */}
+            <Route exact path='/' component={Home}/>
             <Route path='/signup' component={SignUpForm} />
             <Route path='/login' component={LogInForm} />
             {/* <Route path="/logout" component={} /> */}
---
+++ src/components/layout/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './Header';
+import NumbersThreeCols from './PlatformStatistics';
+
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <NumbersThreeCols />
+        </>
+    )
+}
+
+export default Home
